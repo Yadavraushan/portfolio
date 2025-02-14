404f14d1b83afa4df67162238cc7538328017dd4
Yadavraushan <heartkingrajraushan@gmail.com>
2025-02-14 22:22:08 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text ', {
+    strings: [ "Web Developer", "AI Enthusiast", "Machine Learning Engineer" ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 500,
+    loop: true
+} )
